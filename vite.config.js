fbf17efa725c6b221700c92901ffd171c333ae65
Fahmy-Khairy <167533961+Fahmy-Khairy@users.noreply.github.com>
2024-10-15 13:48:42 +0300

    one commit

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/E-commerce/',
   plugins: [react()],
-  server:{
-    // porgit t:3333,
-    host:true
-  }
+  // server:{
+  //   // porgit t:3333,
+  //   host:true
+  // }
 })
